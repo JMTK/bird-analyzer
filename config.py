9ad1b345c4129763a96a1c31fe9146a3baed7971
Jake Morrison <jake.morrison24@gmail.com>
2024-12-30 15:22:13 -0500

    Make config be example only

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-elasticsearch_host = "https://localhost:9200"
-elasticsearch_user = "elastic"
-elasticsearch_password = "Dzz5Jezj4krwnqcM8cQn"
-cert_loc = os.path.join(os.getcwd(), "http_ca.crt")
-
-api_key="8071c1f1-351c-438d-8650-1d47bd95442c"
